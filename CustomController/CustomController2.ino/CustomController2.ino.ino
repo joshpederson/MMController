e94bc7a84a5e7498596991f0de2e76d9cf5be298
joshpederson <pedersonj8674@my.uwstout.edu>
2023-12-06 23:14:19 -0600

    Controller 2 code and update
    Need to finish updating controller 1 code

--- CustomController/CustomController2.ino/CustomController2.ino.ino
+++ CustomController/CustomController2.ino/CustomController2.ino.ino
@@ -5,7 +5,7 @@ const int knockSensor = A3;  // the piezo is connected to analog pin 0
 const int knockSensor2 = A2;  // the piezo is connected to analog pin 1
 const int knockSensor3 = A1;  // the piezo is connected to analog pin 2
 const int knockSensor4 = A0;  // the piezo is connected to analog pin 3
-const int threshold = 20;   // threshold value to decide when the detected sound is a knock or not
+const int threshold = 40;   // threshold value to decide when the detected sound is a knock or not
 
 
 // these variables will change:
@@ -14,6 +14,10 @@ int sensorReading2 = 0;
 int sensorReading3 = 0;
 int sensorReading4 = 0;  // variable to store the value read from the sensor pin
 int ledState = LOW;     // variable used to store the last LED status, to toggle the light
+bool key1 = false;
+bool key2 = false;
+bool key3 = false;
+bool key4 = false;
 
 void setup() {
   pinMode(ledPin, OUTPUT);  // declare the ledPin as as OUTPUT
@@ -29,7 +33,7 @@ void loop() {
   sensorReading4 = analogRead(knockSensor4);
 
   // if the sensor reading is greater than the threshold:
-  if (sensorReading >= threshold) {
+  if (sensorReading >= threshold && !key1) {
     // toggle the status of the ledPin:
     ledState = !ledState;
     // update the LED pin itself:
@@ -38,9 +42,10 @@ void loop() {
     Serial.println(sensorReading);
     //Keyboard.write('a');
     Keyboard.press('j');
+    key1 = true;
   }
 
-  if (sensorReading2 >= threshold) {
+  if (sensorReading2 >= threshold && !key2) {
     // toggle the status of the ledPin:
     ledState = !ledState;
     // update the LED pin itself:
@@ -48,9 +53,10 @@ void loop() {
     // send the string "Knock!" back to the computer, followed by newline
     Serial.println(sensorReading2);
     Keyboard.press('k');
+    key2 = true;
   }
 
-  if (sensorReading3 >= threshold) {
+  if (sensorReading3 >= threshold && !key3) {
     // toggle the status of the ledPin:
     ledState = !ledState;
     // update the LED pin itself:
@@ -58,9 +64,10 @@ void loop() {
     // send the string "Knock!" back to the computer, followed by newline
     Serial.println(sensorReading3);
     Keyboard.press('l');
+    key3 = true;
   }
 
-  if (sensorReading4 >= threshold) {
+  if (sensorReading4 >= threshold && !key4) {
     // toggle the status of the ledPin:
     ledState = !ledState;
     // update the LED pin itself:
@@ -68,8 +75,27 @@ void loop() {
     // send the string "Knock!" back to the computer, followed by newline
     Serial.println(sensorReading4);
     Keyboard.press(';');
+    key4 = true;
   }
   
-  delay(20);  // delay to avoid overloading the serial port buffer
-  Keyboard.releaseAll();
+  if(sensorReading <= 0 && key1){
+    Keyboard.release('j');
+    key1 = false;
+  }
+
+  if(sensorReading2 <= 0 && key2){
+    Keyboard.release('k');
+    key2 = false;
+  }
+
+  if(sensorReading3 <= 0 && key3){
+    Keyboard.release('l');
+    key3 = false;
+  }
+
+  if(sensorReading4 <= 0 && key4){
+    Keyboard.release(';');
+    key4 = false;
+  }
+  delay(20);
 }
